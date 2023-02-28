9aff37a55993f46fba9873251fb93924ce41df16
Paulo Ricardo F. S. Junior <pauloricardo.sistemas@hotmail.com>
2023-02-28 20:45:56 -0300

    Add Fastify

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-console.log("OK")
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/', () => {
+    return 'Hello World'
+})
+
+
+app.listen({
+    port: 3333,
+}).then( () => {
+    console.log('OK')
+})
